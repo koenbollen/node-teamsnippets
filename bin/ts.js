55e2721246166de1ab2692837488875c8f1845ba
Koen Bollen <meneer@koenbollen.nl>
2014-04-07 14:25:58 +0200

    Implements prompting for settings if missing them.

--- bin/ts.js
+++ bin/ts.js
@@ -4,10 +4,31 @@ var _ = require('underscore');
 var fs = require('fs');
 var program = require('commander');
 var tilde = require('tilde-expansion');
+var prompt = require('prompt');
 
 var ts = require('../lib/ts');
 
-function store(filepath, callback) {
+var package_json = require('../package.json');
+
+var settings_schema = {
+    properties: {
+      from: {
+        type: 'string',
+        description: 'Your TeamSnippets.com e-mail',
+        pattern: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        message: 'this is not an e-email address',
+        required: true
+      },
+      team: {
+        conform: parseInt,
+        description: 'Your TeamSnippets team number',
+        message: 'must be a number',
+        required: true
+      }
+    }
+  };
+
+function store(filepath, valid, callback) {
   tilde(filepath, function(filepath) {
     var store;
     try {
@@ -16,22 +37,49 @@ function store(filepath, callback) {
       if(e.code && e.code == 'MODULE_NOT_FOUND') {
         console.error('warning: no config file, using defaults.');
         store = {
-          records: []
+          records: [],
+          history: []
         };
       } else {
         throw e;
       }
     }
-    callback(store, function(changed) {
-      if(changed !== false) {
-        fs.writeFileSync(filepath, JSON.stringify(store));
-      }
-    });
+
+    if(!store.records) store.records = [];
+    if(!store.history) store.history = [];
+
+    var run = function run() {
+      callback(store, function(changed) {
+        if(changed !== false) {
+          fs.writeFileSync(filepath, JSON.stringify(store, null, 2));
+        }
+      });
+    };
+
+    if(valid && (!store.from || !store.team)) {
+      prompt.colors = false;
+      prompt.start();
+      if(store.from) delete settings_schema.properties.from;
+      if(store.team) delete settings_schema.properties.team;
+      prompt.get(settings_schema, function(err, result) {
+        if(err) {
+          console.error(err);
+          process.exit(1);
+        }
+        store.from = result.from;
+        store.team = result.team;
+        fs.writeFileSync(filepath, JSON.stringify(store, null, 2));
+        run();
+      });
+    } else {
+      run();
+    }
   });
 }
 
 program
-  .version('0.0.0')
+  .version(package_json.version)
+  .description(package_json.description)
   .option('-v, --verbose', 'display what\'s going on')
   .option('-q, --quiet', 'suppress all output (expect for error\'s)')
   .option('-c, --config <file>', 'config/store file to use [~/.teamsnippets.json]', '~/.teamsnippets.json')
@@ -40,9 +88,9 @@ program
     console.log( '    $ ts Wrote code');
     console.log( '    $ ts Has a meeting about coffee');
     console.log( '    $ ts stage');
-    console.log( '    $ ts send\n');
+    console.log( '    $ ts post\n');
 
-    console.log( '  Protip:\n\n    Change your teamsnippts settings to use Markdown!\n');
+    console.log( '  Protip:\n\n    Change your TeamSnippets.com settings to use Markdown!\n');
   })
   .on('--verbose', function() {
     console.log('asd');
@@ -54,19 +102,35 @@ program
   //.option('-y, --yes', 'send without confirmation')
   .option('-d, --dryrun', 'don\'t accually send any data')
   .action(function(options){
-    store(program.config, function(store, done) {
-      ts.postupdate(store.records, {
-        from: store.from,
-        team: store.team,
-        dryrun: options.dryrun
-      }, function(err, result) {
-        if(err) {
-          console.error('error: failed to post update: ' + err.message );
-          process.exit(1);
+    store(program.config, true, function(store, done) {
+
+      if(store.records.length == 0) {
+        console.log('# Your TeamSnippets stage is currently clean, get workin\'!')
+      } else {
+        if(program.verbose) {
+          console.log('# Posting the following update to team ' + store.team + ':');
+          console.log(ts.format_post(store.records)+'\n');
         }
-        store.records = [];
-        done(!options.dryrun);
-      });
+        ts.postupdate(store.records, {
+          from: store.from,
+          team: store.team,
+          dryrun: options.dryrun
+        }, function(err, result) {
+          if(err) {
+            console.error('error: failed to post update: ' + err.message );
+            process.exit(1);
+          }
+          store.history.push({
+            posted: new Date(),
+            records: store.records,
+          });
+          store.records = [];
+          if(program.verbose) {
+            console.log(options.dryrun ? '# not sent' : '# sent');
+          }
+          done(!options.dryrun);
+        });
+      }
     });
   });
 
@@ -74,9 +138,13 @@ program
   .command('stage')
   .description('list record currently on stage')
   .action(function(options){
-    store(program.config, function(store, done) {
-      console.log('stage');
-      console.log(store);
+    store(program.config, false, function(store, done) {
+      if(store.records.length == 0) {
+        console.log('# Your TeamSnippets stage is currently clean, get workin\'!')
+      } else {
+        console.log('# TeamSnippets currently on stage:\n');
+        console.log(ts.format_post(store.records)+'\n');
+      }
       done(false);
     });
   });
@@ -88,7 +156,7 @@ program
     var args = _.toArray(arguments);
     args.pop();
     var line = args.join(' ');
-    store(program.config, function(store, done) {
+    store(program.config, false, function(store, done) {
       store.records.push({text: line, date: new Date()});
       done(true);
     });
